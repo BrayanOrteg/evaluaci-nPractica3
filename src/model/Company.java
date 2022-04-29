f75d024720ee2c975e4485537ac41d722a1c1ccb
BrayanOrteg <brayansteven0322@gmail.com>
2022-04-29 12:56:04 -0500

    upload the rest of the code

--- src/model/Company.java
+++ src/model/Company.java
@@ -37,17 +37,46 @@ public class Company{
     }
 
     public String showTotalBatteries() {
-    	return "";
+    	 int numBatteries =0;
+         int numRechargeableBatteries =0;
+         for(int i =0; i<MAX_BATTERIES; i++){
+            if (batteries [i] != null && batteries[i] instanceof Battery){
+                numBatteries ++;
+            }else{
+                numRechargeableBatteries ++;
+            }
+         }  
+
+        return "El numero de baterias tradicionales es: " + numBatteries + "\n El numero de baterias recargables es: "+ numRechargeableBatteries;
     }
     
     public String showBatteriesInfo() {
     	String str = "";
+
+        for (int i =0; i<MAX_BATTERIES; i++){
+            if(batteries[i]!=null){
+                str+= batteries[i].toString()+ "\n";
+            }
+        }
     	return str;
     }
     
 
 	public double calculateUsefulPromLifeCost(){
-		return 0.0;
+		int numOfBatteriesRechargeable=0;
+        double prom=0;
+        double sum=0;
+
+        for (int i= 0; i<MAX_BATTERIES;i++){
+            if(batteries[i]!=null && batteries[i] instanceof RechargeableBattery){
+                sum += ((RechargeableBattery)batteries[i]).calculateUseFullLifeCost();
+                numOfBatteriesRechargeable++;
+            }
+        }
+
+        prom=sum/numOfBatteriesRechargeable;
+
+        return prom;
 	}
 
 }
